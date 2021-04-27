9b8d205c05cdfdd6dffbee79b8e251c34135cf32
Cyberxuz <otash2280gmail.com>
2021-04-27 15:30:28 +0500

    first2

--- app/src/main/java/com/example/dizayn/MainActivity.java
+++ app/src/main/java/com/example/dizayn/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.dizayn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView  imageView1;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.drawer1);
+
+        imageView1=findViewById(R.id.drawermenu);
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
